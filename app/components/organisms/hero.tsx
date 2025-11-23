7a683dbd143f1d44362517302d5888e5f9b843e4
Toni <dinca.andi@gmail.com>
2025-11-23 13:29:37 +0100

    added element positions in hero

--- app/components/organisms/hero.tsx
+++ app/components/organisms/hero.tsx
@@ -1,5 +1,16 @@
 let Hero = () => {
-  return <div className="sectionGlassBackground">whatever</div>;
+  return (
+    <div className="sectionGlassBackground">
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between">
+          <div>Logo</div>
+          <div>EN DE RO</div>
+        </div>
+        <div>FullStack Developer</div>
+        <div>Hero Description</div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
