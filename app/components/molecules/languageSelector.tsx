3be74b6aacf166b2ffd7e1f02ca8ca815882bc26
Toni <dinca.andi@gmail.com>
2025-11-26 11:41:34 +0100

    language selector element style added

--- app/components/molecules/languageSelector.tsx
+++ app/components/molecules/languageSelector.tsx
@@ -2,7 +2,7 @@ import DefaultButton from "../atoms/defaultButton";
 
 let LanguageSelector = () => {
   return (
-    <div>
+    <div className="flex justify-items-start gap-[10px] bg-white/15 rounded-[50px] p-[5px]">
       <DefaultButton>EN</DefaultButton>
       <DefaultButton>DE</DefaultButton>
       <DefaultButton>RO</DefaultButton>
